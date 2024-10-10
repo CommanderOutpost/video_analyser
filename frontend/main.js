4dcfc2c63d7c0b92038d5d62f8b9bf45d7246e2d
OlaleyeFisayo <festusolaleyef@gmail.com>
2024-10-10 14:35:12 +0100

    can now display when app is built

--- frontend/main.js
+++ frontend/main.js
@@ -2,21 +2,10 @@ import { app, BrowserWindow } from "electron";
 import * as path from "path";
 import { fileURLToPath } from "url";
 
-const isDev = !app.isPackaged; // Check if we're in development or production
-let mainWindow
+let mainWindow;
 // // Manually define __dirname in ESM
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-// Enable electron-reload in development mode
-if (isDev) {
-  const electronPath = path.resolve(__dirname, "node_modules/.bin/electron");
-  import("electron-reload").then((module) => {
-    module.default(__dirname, {
-      electron: electronPath,
-    });
-  });
-}
 
 function createWindow() {
   const preloadPath = path.resolve(__dirname, "preload.js");
@@ -29,18 +18,12 @@ function createWindow() {
     webPreferences: {
       preload: preloadPath,
       nodeIntegration: true,
-      contextIsolation: false,
+      contextIsolation: true,
       enableRemoteModule: false,
     },
   });
 
-  if (isDev) {
-    // In development, load the Vite dev server
-    mainWindow.loadURL("http://localhost:5173/");
-  } else {
-    // In production, load the generated `index.html` from the `dist` folder
-    mainWindow.loadFile(path.join(__dirname, "dist/index.html"));
-  }
+  mainWindow.loadFile(path.join(__dirname, "dist/index.html"));
 }
 
 app.whenReady().then(() => {
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./",
   build: {
     outDir: "dist",
-    assetsDir: 'assets',
-    sourcemap: true,
-    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+    },
   },
 });
